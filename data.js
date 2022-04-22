dab1ac5bc2e008626dd5fd7aa30ee509af456f89
Ednial <ericxonb@gmail.com>
2022-04-22 16:29:21 -0500

    Actualicacion: Se agrega logica puntaje-pregutanas

--- data.js
+++ data.js
@@ -1,35 +1,157 @@
 
 let data = [
     {
-        question: "pregunta 1",
-        options: {opt1: "1opción 1", opt2: "1opción 2", opt3:"1opción 3", opt4: "1opción 4"},
-        answer: "opt3",
+        question: "¿Cuál es el río más largo del mundo?",
+        options: {opt1: "1opción 1", opt2: "1opción 2", opt3:"Amazonas", opt4: "1opción 4"},
+        answer: "Amazonas",
         points: 100
     },
     {
         question: "pregunta 2",
         options: {opt1: "2opción 1", opt2: "2opción 2", opt3:"2opción 3", opt4: "2opción 4"},
-        answer: "opt4",
+        answer: "2opción 4",
         points: 100
     },
     {
         question: "pregunta 3",
         options: {opt1: "3opción 1", opt2: "3opción 2", opt3:"3opción 3", opt4: "3opción 4"},
-        answer: "opt2",
+        answer: "3opción 2",
         points: 100
     },
     {
         question: "pregunta 4",
         options: {opt1: "4opción 1", opt2: "4opción 2", opt3:"4opción 3", opt4: "4opción 4"},
-        answer: "opt3",
+        answer: "4opción 3",
         points: 100
     },
     {
         question: "pregunta 5",
         options: {opt1: "5opción 1", opt2: "5opción 2", opt3:"5opción 3", opt4: "5opción 4"},
-        answer: "opt1",
+        answer: "5opción 1",
         points: 100
     },
+    {
+        question: "pregunta 6",
+        options: {opt1: "6opción 1", opt2: "6opción 2", opt3:"6opción 3", opt4: "6opción 4"},
+        answer: "6opción 3",
+        points: 200
+    },
+    {
+        question: "pregunta 7",
+        options: {opt1: "7opción 1", opt2: "7opción 2", opt3:"7opción 3", opt4: "7opción 4"},
+        answer: "7opción 4",
+        points: 200
+    },
+    {
+        question: "pregunta 8",
+        options: {opt1: "8opción 1", opt2: "8opción 2", opt3:"8opción 3", opt4: "8opción 4"},
+        answer: "8opción 2",
+        points: 200
+    },
+    {
+        question: "pregunta 9",
+        options: {opt1: "9opción 1", opt2: "9opción 2", opt3:"9opción 3", opt4: "9opción 4"},
+        answer: "9opción 3",
+        points: 200
+    },
+    {
+        question: "pregunta 10",
+        options: {opt1: "10opción 1", opt2: "10opción 2", opt3:"10opción 3", opt4: "10opción 4"},
+        answer: "10opción 1",
+        points: 200
+    },
+    // Otras 10 preguntas
+    {
+        question: "pregunta 11",
+        options: {opt1: "11opción 1", opt2: "11opción 2", opt3:"11opción 3", opt4: "11opción 4"},
+        answer: "11opción 3",
+        points: 300
+    },
+    {
+        question: "pregunta 12",
+        options: {opt1: "12opción 1", opt2: "12opción 2", opt3:"12opción 3", opt4: "12opción 4"},
+        answer: "12opción 4",
+        points: 300
+    },
+    {
+        question: "pregunta 13",
+        options: {opt1: "13opción 1", opt2: "13opción 2", opt3:"13opción 3", opt4: "13opción 4"},
+        answer: "13opción 2",
+        points: 300
+    },
+    {
+        question: "pregunta 14",
+        options: {opt1: "14opción 1", opt2: "14opción 2", opt3:"14opción 3", opt4: "14opción 4"},
+        answer: "14opción 3",
+        points: 300
+    },
+    {
+        question: "pregunta 15",
+        options: {opt1: "15opción 1", opt2: "15opción 2", opt3:"15opción 3", opt4: "15opción 4"},
+        answer: "15opción 1",
+        points: 300
+    },
+    {
+        question: "pregunta 16",
+        options: {opt1: "16opción 1", opt2: "16opción 2", opt3:"16opción 3", opt4: "16opción 4"},
+        answer: "16opción 3",
+        points: 400
+    },
+    {
+        question: "pregunta 17",
+        options: {opt1: "17opción 1", opt2: "17opción 2", opt3:"17opción 3", opt4: "17opción 4"},
+        answer: "17opción 4",
+        points: 400
+    },
+    {
+        question: "pregunta 18",
+        options: {opt1: "18opción 1", opt2: "18opción 2", opt3:"18opción 3", opt4: "18opción 4"},
+        answer: "18opción 2",
+        points: 400
+    },
+    {
+        question: "pregunta 19",
+        options: {opt1: "19opción 1", opt2: "19opción 2", opt3:"19opción 3", opt4: "19opción 4"},
+        answer: "19opción 3",
+        points: 400
+    },
+    {
+        question: "pregunta 20",
+        options: {opt1: "20opción 1", opt2: "20opción 2", opt3:"20opción 3", opt4: "20opción 4"},
+        answer: "20opción 1",
+        points: 400
+    },
+    //Otras 5 preguntas.
+    {
+        question: "pregunta 21",
+        options: {opt1: "21opción 1", opt2: "21opción 2", opt3:"21opción 3", opt4: "21opción 4"},
+        answer: "21opción 1",
+        points: 500
+    },
+    {
+        question: "pregunta 22",
+        options: {opt1: "22opción 1", opt2: "22opción 2", opt3:"22opción 3", opt4: "22opción 4"},
+        answer: "22opción 3",
+        points: 500
+    },
+    {
+        question: "pregunta 23",
+        options: {opt1: "23opción 1", opt2: "23opción 2", opt3:"23opción 3", opt4: "23opción 4"},
+        answer: "23opción 4",
+        points: 500
+    },
+    {
+        question: "pregunta 24",
+        options: {opt1: "24opción 1", opt2: "24opción 2", opt3:"24opción 3", opt4: "24opción 4"},
+        answer: "24opción 2",
+        points: 500
+    },
+    {
+        question: "pregunta 25",
+        options: {opt1: "25opción 1", opt2: "25opción 2", opt3:"25opción 3", opt4: "25opción 4"},
+        answer: "25opción 3",
+        points: 500
+    }    
 ];
 
 export default data;
