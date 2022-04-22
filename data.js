d4abfeb33767f25e82736d5c45ba8948fcd269a3
Jehison Galarza <jehisonsc045@gmail.com>
2022-04-22 02:25:14 -0500

    DOM Ronda 1 --- Manipulación del DOM para mostrar la primera pregunta y sus opciones

--- data.js
+++ data.js
@@ -1,33 +1,35 @@
 
-const data = [
+let data = [
     {
         question: "pregunta 1",
-        options: {opt1: "opción 1", opt2: "opción 2", opt3:"opción 3", opt4: "opción 4"},
+        options: {opt1: "1opción 1", opt2: "1opción 2", opt3:"1opción 3", opt4: "1opción 4"},
         answer: "opt3",
         points: 100
     },
     {
         question: "pregunta 2",
-        options: {opt1: "opción 1", opt2: "opción 2", opt3:"opción 3", opt4: "opción 4"},
+        options: {opt1: "2opción 1", opt2: "2opción 2", opt3:"2opción 3", opt4: "2opción 4"},
         answer: "opt4",
         points: 100
     },
     {
         question: "pregunta 3",
-        options: {opt1: "opción 1", opt2: "opción 2", opt3:"opción 3", opt4: "opción 4"},
+        options: {opt1: "3opción 1", opt2: "3opción 2", opt3:"3opción 3", opt4: "3opción 4"},
         answer: "opt2",
         points: 100
     },
     {
         question: "pregunta 4",
-        options: {opt1: "opción 1", opt2: "opción 2", opt3:"opción 3", opt4: "opción 4"},
+        options: {opt1: "4opción 1", opt2: "4opción 2", opt3:"4opción 3", opt4: "4opción 4"},
         answer: "opt3",
         points: 100
     },
     {
         question: "pregunta 5",
-        options: {opt1: "opción 1", opt2: "opción 2", opt3:"opción 3", opt4: "opción 4"},
+        options: {opt1: "5opción 1", opt2: "5opción 2", opt3:"5opción 3", opt4: "5opción 4"},
         answer: "opt1",
         points: 100
     },
-]
+];
+
+export default data;
--- logica.js
+++ logica.js
@@ -1,5 +1,68 @@
+import data from './data.js'
 
+window.onload = () =>{
+    let random = getRandom(5,0);
+    
+    sessionStorage.setItem('actualLevel', 1);
+        
+    levelSpace.innerHTML = "Ronda "+  level;
+    getQuestion(random)
+    getOptions(random)
 
+}
 
 let gameZone = document.querySelector("#game-zone"),
-    pointsZone = document.querySelector("#pointsZone");
+    questionZone = document.querySelector("#question"),
+    optionsZone = document.querySelector("#options"),
+    pointsZone = document.querySelector("#pointsZone"),
+    levelSpace = document.querySelector("#level"),
+    level = String(sessionStorage.getItem('actualLevel')),
+    btnContinue = document.querySelector("#btn-continue");
+
+
+btnContinue.addEventListener('click', ()=>{
+        let random = getRandom(4,0)
+        alert(random);
+        borraGameZone(); 
+        getOptions(random)
+    }
+)
+
+
+
+function borraGameZone(){
+    gameZone.innerHTML = "";
+}
+
+function getQuestion(random){
+    let question = data[random].question;
+    questionZone.innerHTML = question
+    
+}
+
+function getOptions(random){
+    let options = Object.values(data[random].options)
+    optionsZone.innerHTML = ""
+    let htmlInsert =`<div class="row mb-1">
+                        <div class="input-group input-group-md m-4">
+                            <div class="input-group-text input-group-md">
+                                <select class="form-select form-select-md" aria-label="Default select example" id="select-option">
+                                `
+
+    for(let i = 0; i <= options.length-1; i++){
+        htmlInsert += `<option value="${options[i]}">${options[i]}</option>`;
+    }
+
+    htmlInsert += `
+            </select>
+        </div>
+        
+    </div>
+</div>`
+    optionsZone.innerHTML = htmlInsert
+    
+}
+
+function getRandom(max, min){
+    return Math.floor(Math.random() * (max - min) + min);
+}
